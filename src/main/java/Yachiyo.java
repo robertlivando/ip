ee863763fc094d94e3db1d7637fb7952b531ef1c
robertlivando <robertlivando@gmail.com>
2026-08-16 22:24:35 +0800

    Rename Duke.java and update banner

---
+++ src/main/java/Yachiyo.java
@@ -0,0 +1,10 @@
+public class Yachiyo {
+    public static void main(String[] args) {
+        String banner = "__   __    _      ____   _   _   _____  __   __   ___  \n"
+                        + "\\ \\ / /   / \\    / ___| | | | | |_   _| \\ \\ / /  / _ \\ \n"
+                        + " \\ V /   / _ \\  | |     | |_| |   | |    \\ V /  | | | |\n"
+                        + "  | |   / ___ \\ | |___  |  _  |  _| |_    | |   | |_| |\n"
+                        + "  |_|  /_/   \\_\\ \\____| |_| |_| |_____|   |_|    \\___/ \n";
+        System.out.println(banner);
+    }
+}
