70c1490f681c3e9f9bd13e82d3b56164322784cf
robertlivando <robertlivando@gmail.com>
2026-08-16 22:32:00 +0800

    Add greeting and exit

--- src/main/java/Yachiyo.java
+++ src/main/java/Yachiyo.java
@@ -5,6 +5,11 @@ public class Yachiyo {
                         + " \\ V /   / _ \\  | |     | |_| |   | |    \\ V /  | | | |\n"
                         + "  | |   / ___ \\ | |___  |  _  |  _| |_    | |   | |_| |\n"
                         + "  |_|  /_/   \\_\\ \\____| |_| |_| |_____|   |_|    \\___/ \n";
+        String greeting = "Ohayo! Yachiyo here!\n" + "What shall we create today?";
+        String exit = "Until we meet again—have a lovely day!";
+
         System.out.println(banner);
+        System.out.println(greeting);
+        System.out.println(exit);
     }
 }
