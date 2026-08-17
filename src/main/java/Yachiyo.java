79ef35f516484af8e2fb8fef948f21baa8154825
robertlivando <robertlivando@gmail.com>
2026-08-17 22:29:14 +0800

    Add support for echo

--- src/main/java/Yachiyo.java
+++ src/main/java/Yachiyo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Yachiyo {
     public static void main(String[] args) {
         String banner = "__   __    _      ____   _   _   _____  __   __   ___  \n"
@@ -5,11 +7,37 @@ public class Yachiyo {
                         + " \\ V /   / _ \\  | |     | |_| |   | |    \\ V /  | | | |\n"
                         + "  | |   / ___ \\ | |___  |  _  |  _| |_    | |   | |_| |\n"
                         + "  |_|  /_/   \\_\\ \\____| |_| |_| |_____|   |_|    \\___/ \n";
-        String greeting = "Ohayo! Yachiyo here!\n" + "What shall we create today?\n";
+        String greeting = "Ohayo! Yachiyo here!\n" + "What shall we create today?";
         String exit = "Until we meet again—have a lovely day!";
 
+        // Introduction (banner + greeting)
         System.out.println(banner);
         System.out.println(greeting);
-        System.out.println(exit);
+        System.out.println("========================================================\n");
+
+        // Echo
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            // Skip empty inputs
+            if (command.isEmpty()) {
+                continue;
+            }
+
+            // Exit when user types bye
+            if (command.equals("bye")) {
+                System.out.println("========================================================");
+                System.out.println(exit);
+                System.out.println("========================================================\n");
+                break;
+            } else {
+                // Echo
+                System.out.println("========================================================");
+                System.out.println(command);
+                System.out.println("========================================================\n");
+            }
+        }
+        scanner.close();
     }
 }
